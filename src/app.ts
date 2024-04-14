9cdfdeac127e63963504d36ac1d5e87f7e82875f
Juano <juanjosemunozva@gmail.com>
2024-04-14 00:43:02 -0500

    CORS

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,12 @@ const PORT: number = 3000;
 app.use(cors({
     origin : ['http:localhost:5173','https://movin-out.vercel.app']
 }))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific HTTP methods
+  next();
+});
 app.use(morgan('dev'))
 app.use(express.json())
 app.use('/api/v1/users', userRouter)
