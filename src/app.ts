7e425985b9c1d34902cd5edd22da5e1bae6eee5c
Juano <juanjosemunozva@gmail.com>
2024-04-14 00:28:34 -0500

    CORS

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,11 @@ import express, { Express } from 'express';
 import userRouter from './routes/user.routes';
 import todoRouter from './routes/todo.routes';
 import morgan from 'morgan'
-import cors from 'cors'
+import cors, { CorsOptions } from 'cors'
 import './database'
 const app: Express = express()
 const PORT: number = 3000;
-app.use(cors())
+app.use(cors(['https://movin-out.vercel.app','http:localhost:5173'] as CorsOptions))
 app.use(morgan('dev'))
 app.use(express.json())
 app.use('/api/v1/users', userRouter)
